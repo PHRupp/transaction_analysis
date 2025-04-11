4c26fba9da02064b6d71b128dcbcc4d6641c22b2
PHRupp <pat.h.rupp@gmail.com>
2025-04-11 17:41:33 -0500

    successful parsing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
+import re
+import traceback as tb
+
 import numpy as np
 import pandas as pd
+
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List
@@ -24,6 +28,8 @@ section_title = "Hunter's Creek"
 section_invoice_paid = 'Invoice Paid'
 section_end_run_date = 'Run Date'
 
+PHONE_NUMBER_PATTERN = re.compile('\(\d{3}\) \d{3}-\d{4}')
+
 # Some of the data is shifted right
 section_row_indices1 = np.where(df[title_col1] == section_invoice_paid)[0]
 section_row_indices2 = np.where(df[title_col2] == section_invoice_paid)[0]
@@ -31,6 +37,9 @@ section_end_row_indices = np.where(df[run_date_col] == section_end_run_date)[0]
 section_row_indices = np.concatenate( (section_row_indices1, section_row_indices2))
 section_row_indices.sort()
 print(section_row_indices)
+
+
+data_frames = []
 
 
 @dataclass
@@ -46,6 +55,18 @@ class DataRow:
     PhoneNumber: str
 
 
+"""
+						Hunter's Creek						
+			3964 Town Center Blvd, Orlando,FL 32837-6103									
+						Invoice Paid						
+	Date	Time					FoP		Invoice	Type	Qty	Amount
+	9/4/2024	4:51 PM					Debit		A104091	D	12	$71.71 
+	689	272-5740	TSIFTSI SEVNTA									
+	9/4/2024	5:03 PM					CASH		A104656	D	1	$32.03 
+	407	376-7264	GUSMAN ERIC									
+	9/4/2024	5:34 PM					Debit		A104528	L	6	$30.29 
+	714	244-5456	hache nancy																	
+"""
 def parse_section(
     section_row_index: int,
     section_end_row_index: int,
@@ -65,24 +86,26 @@ def parse_section(
 
     # keep parsing each transaction
     while not reached_end:
-        print('------------------')
-        print(data_row_index)
-        data_row = parse_transaction(data_row_index, col_name_indices, df)
-        data.append(data_row)
-        #print(data)
-
-        # jump to next transaction
-        data_row_index += 2
 
         # Sometimes there are two invoices under one transaction
         # see if it's a date next then skip
+        """
+        9/4/2024	11:12 AM					Debit		A95917	D	2	$0.00 
+        9/4/2024	11:12 AM					Debit		A104416	D	1	$0.00 
+        954	849-5156	GOLDSMITH JOHN									
+        """
         try:
-            area_code_index = col_name_indices[0]
-            area_code = df.iat[data_row_index, area_code_index]
-            date = datetime.strptime(area_code, "%m/%d/%Y")
-            data_row_index += 1
+            # skip line, this may happen if a transaction is not parsable
+            if is_parsable_transaction(data_row_index, col_name_indices, df):
+                data_row = parse_transaction(data_row_index, col_name_indices, df)
+                data.append(data_row)
+
         except Exception as e:
+            #print(tb.format_exc())
             pass
+
+        # Move forward a row
+        data_row_index += 1
 
         # Jump out if we reached the end of section
         if (data_row_index+1) >= section_end_row_index:
@@ -91,6 +114,48 @@ def parse_section(
     return data
 
 
+"""
+Example Transaction:
+9/3/2024	8:47 AM					Debit		A103844	L	4	$23.96 
+407	937-9448	PORCELLA STEFANO									
+"""
+def is_parsable_transaction(
+    data_row_index: int,
+    col_name_indices: List[int],
+    df: pd.DataFrame,
+) -> bool:
+
+    success = False
+
+    try:
+        # Verify line 1 starts with a valid date
+        area_code_index = col_name_indices[0]
+        area_code = df.iat[data_row_index, area_code_index]
+        date = datetime.strptime(area_code, "%m/%d/%Y")
+
+        # Verify the phone number is parsable
+        area_code_index = col_name_indices[0]
+        area_code = df.iat[data_row_index+1, area_code_index]
+        other_phone_digits = df.iat[data_row_index+1, area_code_index+1]
+        phone_number = f'({area_code}) {other_phone_digits}'
+
+        match = PHONE_NUMBER_PATTERN.search(phone_number)
+
+        if match is not None:
+            success = True
+
+    except Exception as e:
+        #print(tb.format_exc())
+        pass
+
+    return success
+
+
+"""
+Example Transaction:
+9/3/2024	8:47 AM					Debit		A103844	L	4	$23.96 
+407	937-9448	PORCELLA STEFANO									
+"""
 def parse_transaction(
     data_row_index: int,
     col_name_indices: List[int],
@@ -99,39 +164,39 @@ def parse_transaction(
 
     # Parse the phone number
     area_code_index = col_name_indices[0]
-    area_code = df.iat[data_row_index, area_code_index]
-    other_phone_digits = df.iat[data_row_index, area_code_index+1]
+    area_code = df.iat[data_row_index+1, area_code_index]
+    other_phone_digits = df.iat[data_row_index+1, area_code_index+1]
     phone_number = f'({area_code}) {other_phone_digits}'
 
     # Parse date/time
     date_index = col_name_indices[0]
     time_index = col_name_indices[1]
-    date = df.iat[data_row_index+1, date_index]
-    time = df.iat[data_row_index+1, time_index]
+    date = df.iat[data_row_index, date_index]
+    time = df.iat[data_row_index, time_index]
 
     # Parse name
     name_index = time_index + 1
-    name = df.iat[data_row_index, name_index]
+    name = df.iat[data_row_index+1, name_index]
 
     # Parse the FoP
     fop_index = col_name_indices[2]
-    fop = df.iat[data_row_index+1, fop_index]
+    fop = df.iat[data_row_index, fop_index]
 
     # Parse the Invoice number
     invoice_index = col_name_indices[3]
-    invoice = df.iat[data_row_index+1, invoice_index]
+    invoice = df.iat[data_row_index, invoice_index]
 
     # Parse the type
     transaction_type_index = col_name_indices[4]
-    transaction_type = df.iat[data_row_index+1, transaction_type_index]
+    transaction_type = df.iat[data_row_index, transaction_type_index]
 
     # Parse the quantity
     qty_index = col_name_indices[5]
-    qty = int(df.iat[data_row_index+1, qty_index])
+    qty = int(df.iat[data_row_index, qty_index])
 
     # Parse the amount
     amount_index = col_name_indices[6]
-    amount = df.iat[data_row_index+1, amount_index]
+    amount = df.iat[data_row_index, amount_index]
 
     # return the data record
     data_row = DataRow(
@@ -149,18 +214,18 @@ def parse_transaction(
     return data_row
 
 
-
 # process each section (one per day)
 for section_row_index, section_end_row_index in zip(section_row_indices, section_end_row_indices):
-    
-    data = parse_section(section_row_index, section_end_row_index, df)
-    
+
+    section_data = parse_section(section_row_index, section_end_row_index, df)
+
     new_df = pd.DataFrame([
-        d.__dict__ for d in data
+        d.__dict__ for d in section_data
     ])
 
-    print(new_df)
-    exit(0)
+    data_frames.append(new_df)
+
+pd.concat(data_frames).to_csv('final_data.csv')
 
 
 
