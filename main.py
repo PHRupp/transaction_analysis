5705e45163a64b235a061bed301b554d062b4156
PHRupp <pat.h.rupp@gmail.com>
2025-07-05 18:42:01 -0500

    found bug in XLS vs XLSX data parsing

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
 
 import logging
 import os
+import re
 import traceback as tb
 
-from os.path import exists, join
+from os.path import join
+from typing import List
 
 import numpy as np
 import pandas as pd
 
-from parser.file_parser import clean_up, parse_reduced_df, reduce_section
+from parser.file_parser import reduce_file
+from parser.data import DataRow
+
+"""
+WARNING: Processing XLSX files instead of the XLS provides more data. Not sure why, but it 
+parses the XLS and has missing area codes or phone numbers, but it's fully populated in XLSX.
+"""
+
+in_file_names = [
+    '2023Q12.xlsx',
+    '2023Q34.xlsx',
+    '2024Q12.xlsx',
+    '2024Q34.xlsx',
+]
 
 
-#data_dir = 'G:/My Drive/LBA/Dry Cleaning MLX/HC/Analysis'
-data_dir = 'G:/My Drive/LBA/XL SHEET FOR HUNTERS CREEK'
+data_dir = 'G:/My Drive/LBA/MLX Admin/HC/Analysis/6mo Data Sets'
 log_file = './results.log'
-in_file_name = 'INVOICEREPORT2025-RECENT.xls'
-out_file_name = 'HC_2023.csv'
-
-
-if exists(log_file):
-    os.remove(log_file)
-logging.basicConfig(filename=log_file, level=logging.INFO)
-
-
-#in_file = 'C:/Users/pathr/Downloads/SEPT2024-2025#1.xls'
-in_file = join(data_dir, in_file_name)
-out_file = join(data_dir, out_file_name)
-df = pd.read_excel(in_file, header=None)
-
-# Clean up the file otherwise the weird line skips are annoying
-df = clean_up(df)
-#df.to_csv('reduced.csv', index=False)
-
-# Get the section indices
-data_frames = []
-section_ind = df.index[(df == 'Invoice Paid').sum(axis=1) > 0]
-n = len(section_ind)
+out_file_name = 'HC.csv'
 
 try:
-    # process each section (one per day roughly)
-    for section_start, section_end in zip(section_ind[0:(n-1)], section_ind[1:n]):
+    os.remove(log_file)
+except FileNotFoundError:
+    print(tb.format_exc())
 
-        section_data = reduce_section(section_start, section_end, df)
-        data_frames.append(section_data)
+out_file = join(data_dir, out_file_name)
 
-    # put all the data together
-    df = pd.concat(data_frames, ignore_index=True)
+logging.basicConfig(filename=log_file, level=logging.INFO)
 
-    # Get the final dataframe
-    df = parse_reduced_df(df)
+data_frames: List[DataRow] = []
 
-    df.to_csv(out_file, index=False)
+for f in in_file_names:
+    try:
+        in_file = join(data_dir, f)
+        data_frames += reduce_file(in_file)
+    except Exception as e:
+        print('FILE ERROR!')
+        logging.warning(tb.format_exc())
 
-except Exception as e:
-    logging.critical(tb.format_exc())
+# put all the data together
+pd.concat(data_frames, ignore_index=True).to_csv(out_file)
--- parser/file_parser.py
+++ parser/file_parser.py
@@ -4,12 +4,22 @@ import re
 import traceback as tb
 from dataclasses import asdict
 from datetime import datetime
+from os.path import join
 from typing import Any, List
 
 import numpy as np
 import pandas as pd
 
 from parser.data import DataRow
+
+
+# CONSTANTS
+run_date_col = 'Unnamed: 1'
+title_col1 = 'Unnamed: 6'
+title_col2 = 'Unnamed: 7'
+section_title = "Hunter's Creek"
+section_invoice_paid = 'Invoice Paid'
+section_end_run_date = 'Run Date'
 
 PARTIAL_PHONE_NUMBER_PATTERN = '^\d{3}-\d{4}$'
 PHONE_NUMBER_PATTERN = re.compile('\(\d{3}\) \d{3}-\d{4}')
@@ -122,7 +132,7 @@ def reduce_section(
     reduced_df['Name'] = ''
     col_shifted_ind = reduced_df['Time'].str.contains(PARTIAL_PHONE_NUMBER_PATTERN)
     if col_shifted_ind.sum() > 0:
-        reduced_df['Name'] = df.loc[data_row_index:(section_end_row_index-1), 3].to_list()
+        reduced_df['Name'] = df.loc[data_row_index:(section_end_row_index-1), 'Unnamed: 3'].to_list()
     else:
 
         # Otherwise, the time column has the names, parse the names out if doesn't match time format
@@ -135,7 +145,10 @@ def reduce_section(
             reduced_df.loc[i, 'Name'] = None if is_time else time_val
             reduced_df.loc[i, 'Date'] = '000' if is_phone else date_val #not valid area-code, prevents filtering when date column is empty
             reduced_df.loc[i, 'Time'] = time_val if is_time else (date_val if is_phone else None)
-        
+
+    # keep the strings consistent by having upper case
+    reduced_df['Name'] = reduced_df['Name'].str.upper()
+
     # Drop rows where the date column is empty. other rolumns may have
     # data, but if date is empty then we should remove the whole row
     # reduce here because it won't work during the clean up phase.
@@ -143,8 +156,46 @@ def reduce_section(
     first_col = reduced_df.columns[0]
     drop_ind = reduced_df.index[reduced_df[first_col].isna()]
     reduced_df.drop(index=drop_ind, inplace=True)
-    
+
+    # Remove lines representing the person who grabbed the data
+    ind_delete = reduced_df['Date'] == 'IAN'
+    reduced_df.drop(reduced_df.loc[ind_delete,:].index, inplace=True)
+
+    # Data where row has 'Dry Cleaning' and everything after should be destroyed
+    is_end = reduced_df['Date'] == 'Dry Cleaning'
+    if is_end.sum() > 0:
+        stop_ind = reduced_df.loc[is_end, :].index[0]
+        drop_ind = reduced_df.loc[reduced_df.index >= stop_ind, :].index
+        reduced_df.drop(drop_ind, inplace=True)
+
     return reduced_df
+
+
+def reduce_file(in_file: str) -> List[DataRow]:
+    
+    data_frames: List[DataRow] = []
+
+    raw_df = pd.read_excel(in_file)
+
+    # Clean up the file otherwise the weird line skips are annoying
+    raw_df.dropna(how='all', inplace=True, ignore_index=True)
+
+    # Some of the data is shifted right
+    section_row_indices1 = np.where(raw_df[title_col1] == section_invoice_paid)[0]
+    section_row_indices2 = np.where(raw_df[title_col2] == section_invoice_paid)[0]
+    section_end_row_indices = np.where(raw_df[run_date_col] == section_end_run_date)[0]
+    section_row_indices = np.concatenate( (section_row_indices1, section_row_indices2))
+    section_row_indices.sort()
+
+    # process each section (one per day)
+    for section_row_index, section_end_row_index in zip(section_row_indices, section_end_row_indices):
+        try:
+            section_df = reduce_section(section_row_index, section_end_row_index, raw_df)
+            data_frames.append(section_df)
+        except Exception as e:
+            logging.warning(tb.format_exc())
+
+    return data_frames
 
 
 def parse_reduced_df(df: pd.DataFrame) -> pd.DataFrame:
